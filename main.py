4574c4245b17a2e63c7292f28688ff068f54b47a
mugnaidaniele <daniele.mugnai@unfii.it>
2021-07-09 08:57:48 +0200

    add training and validation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 from models import create
 from continuum.datasets import CIFAR100
 from continuum import ClassIncremental
-#from train import train
+from train import train
 from validate import validate
 from utils.ExemplarSet import ExemplarSet
 from utils.utils import get_all_images_per_class
@@ -15,15 +15,11 @@ from utils.feature_selection import perform_selection
 from utils.utils import getTransform, get_dataset
 from torchvision.transforms import transforms
 
-
 import os
 import numpy as np
 import copy
 
-
-
 scheduling = [0, 1]
-
 
 parser = argparse.ArgumentParser(description='Learning unified classifier via rebalancing')
 parser.add_argument('--dataset', type=str, default="cifar100", metavar='BATCH', help='batch size')
@@ -38,18 +34,15 @@ parser.add_argument('--margin', type=float, default=0.01, metavar='LR', help='le
 parser.add_argument('--gamma', type=float, default=0.01, metavar='LR', help='learning rate for cnn')
 parser.add_argument('--k-negatives', type=int, default=70, metavar='BATCH', help='batch size')
 
-
 parser.add_argument('--rehearsal', type=int, default=1, metavar='BATCH', help='batch size')
 parser.add_argument('--selection', type=str, default="closest", metavar='BATCH', help='batch size')
 parser.add_argument("--exR", action="store_true", default=True, help="experience replay")
 parser.add_argument("--cosine", action="store_true", default=False, help="experience replay")
 
-
 args = parser.parse_args()
 model = create("imagenet", 10, args.cosine)
 dataset_train, dataset_test = get_dataset(args.dataset)
 train_transform, val_transform, test_transform = getTransform(args.dataset)
-
 
 scenario_train = ClassIncremental(dataset_train, increment=args.increment, initial_increment=args.start)
 scenario_val = ClassIncremental(dataset_test, increment=args.increment, initial_increment=args.start)
@@ -65,21 +58,22 @@ for task_id, train_taskset in enumerate(scenario_train):
     print(task_id)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     scheduler_lr = MultiStepLR(optimizer, milestones=scheduling, gamma=args.gamma)
-    
+
     if task_id == 0:
         train_loader = DataLoader(train_taskset, batch_size=args.batch_size, shuffle=True)
         memory_loader = None
+        lamda = None
     else:
         train_loader = DataLoader(train_taskset, batch_size=args.batch_size // 2, shuffle=True)
         memory_loader = DataLoader(exemplar_set, batch_size=args.batch_size // 2, shuffle=True)
         lamda = args.lamda_base * np.sqrt(train_taskset.nb_classes / scenario_train[:task_id].nb_classes)
     # best_acc_on_task = 0
-    # for epoch in range(args.n_epochs):
-    #     train(args, train_loader, memory_loader, model, task_id, criterion_cls, previous_net, optimizer,epoch)
-    #     acc_val = validate()
+    for epoch in range(args.n_epochs):
+        train(args, train_loader, memory_loader, model, task_id, criterion_cls, previous_net, optimizer, epoch, lamda)
+        acc_val, loss_val = validate(model, val_loader=None) # passare loader fino a task t
     #     if acc_val > best_acc_on_task:
     #         best_acc_on_task = acc_val
-    if task_id < scenario_train.nb_tasks-1:
+    if task_id < scenario_train.nb_tasks - 1:
         if args.exR:
             for c in train_taskset.get_classes():
                 images_in_c, labels_in_c = get_all_images_per_class(train_taskset, c)
@@ -88,6 +82,3 @@ for task_id, train_taskset in enumerate(scenario_train):
             previous_net = copy.deepcopy(model)
             model.expand_classes(scenario_train.nb_classes)
             model.cuda()
-
-
-
--- validate.py
+++ validate.py
@@ -1,21 +1,36 @@
 import torch
+from utils.AverageMeter import AverageMeter
+from utils.utils import accuracy
 
 
-def validate():
-    pass
+def validate(args, net, val_loader, criterion):
+    acc_meter = AverageMeter()
+    loss_meter = AverageMeter()
+
+    net.cuda()
+    net.eval
+    with torch.no_grad():
+        for inputs, targets in val_loader:
+            inputs = inputs.cuda()
+            _, output = net(inputs)
+            acc_val = accuracy(output, targets, topk=(1,))  # forse mettere anche top5
+            loss = criterion(output, targets)
+            acc_meter.update(acc_val[0].item(), inputs.size(0))
+            loss_meter.update(loss.item(), inputs.size(0))
+    return acc_meter.avg, loss_meter.avg
 
 
 def extract_features(args, net, loader):
     features = None
     net.cuda()
     net.eval()
-    
+
     with torch.no_grad():
         for inputs, targets in loader:
-            #inputs = inputs.cuda(args.device)
+            # inputs = inputs.cuda(args.device)
             inputs = inputs.cuda()
             f, _ = net(inputs)
-            #f = l2_norm(f)
+            # f = l2_norm(f)
             if features is not None:
                 features = torch.cat((features, f), 0)
             else:
@@ -24,10 +39,8 @@ def extract_features(args, net, loader):
     return features.detach().cpu().numpy()
 
 
-
 def l2_norm(input, axis=1):
     norm = torch.norm(input, 2, axis, True)
     output = torch.div(input, norm)
 
     return output
-
